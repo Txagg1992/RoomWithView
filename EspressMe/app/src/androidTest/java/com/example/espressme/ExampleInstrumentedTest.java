b2419acf96cdeccd3f927ffc4853197bf87c8dca
Enoch <curiousaps@gmail.com>
2019-11-06 18:09:39 -0600

    stuff.

---
+++ EspressMe/app/src/androidTest/java/com/example/espressme/ExampleInstrumentedTest.java
@@ -0,0 +1,53 @@
+package com.example.espressme;
+
+import android.content.Context;
+
+import androidx.test.filters.LargeTest;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule(MainActivity.class);
+
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        assertEquals("com.example.espressme", appContext.getPackageName());
+    }
+
+    @Test
+    public void checkButton() {
+        onView(withId(R.id.button))
+                .perform(click())
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void checkText() {
+        onView(withId(R.id.textView))
+                .check(matches(isDisplayed()));
+    }
+}
